fc3754d03fdfae60ab020df24c21facf90d2545e
franguyarmani <fbpeabody@gmail.com>
2018-11-26 18:49:09 -0500

    suppress unchecked types warning for JSONObjects and clean up

--- CSVtoJSON/src/JSONWriter.java
+++ CSVtoJSON/src/JSONWriter.java
@@ -1,32 +1,28 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class JSONWriter {
-	private File dir;
-	
 	
 	JSONWriter(File dir){
-		this.dir = dir;
 	}
 	
 	public void WriteToJSON(File file, List<String[]> data) throws IOException  {
 		JSONArray obj = buildJSON(data);
-		System.out.println("json obj built");
 		writeToFile(file, obj);
 	}
 	
 	
 	
+	@SuppressWarnings("unchecked")
 	private JSONArray buildJSON(List<String[]> data) {
-		data.remove(0);
 		JSONArray entries = new JSONArray();
 		for(String[] person:data) {
 			JSONObject entry = new JSONObject();
@@ -48,9 +44,7 @@ public class JSONWriter {
 	
 	
 	private void writeToFile(File file, JSONArray obj) throws IOException{
-		System.out.println("in write to File");
 		FileWriter w = new FileWriter(file.toString());
-		System.out.println("made FileWriter");
 		Gson pretty = new GsonBuilder().setPrettyPrinting().create();
 		w.write(pretty.toJson(obj));
 		w.close();
