a19a2c10af62308c54f46e9bf0d321ce8a68954b
franguyarmani <fbpeabody@gmail.com>
2018-11-26 10:36:02 -0500

    add Exception for malformed line in csv

---
+++ CSVtoJSON/src/WrongNumberofElementsException.java
@@ -0,0 +1,24 @@
+
+public class WrongNumberofElementsException extends Exception {
+
+
+	public WrongNumberofElementsException(String message) {
+		super(message);
+	}
+
+	public WrongNumberofElementsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WrongNumberofElementsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WrongNumberofElementsException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
