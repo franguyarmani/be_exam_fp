05607ed280e2c1b73600ee720472dfa0251f7427
franguyarmani <fbpeabody@gmail.com>
2018-11-26 12:33:22 -0500

    cleaned up MalformedDataException.java

--- CSVtoJSON/src/MalformedDataException.java
+++ CSVtoJSON/src/MalformedDataException.java
@@ -2,19 +2,6 @@
 public class MalformedDataException extends Exception {
 	int lineNumber;
 
-	public MalformedDataException() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public MalformedDataException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
-	public MalformedDataException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
 	
 	public MalformedDataException(String message, int lineNumber) {
 		super(message);
@@ -27,17 +14,6 @@ public class MalformedDataException extends Exception {
 
 	public void setLineNumber(int lineNumber) {
 		this.lineNumber = lineNumber;
-	}
-
-	public MalformedDataException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public MalformedDataException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
